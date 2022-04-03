66e672fbc7587afce262eba33e5835854991af0e
Brendan Francis <B.Francis@SprengerV.com>
2022-04-03 15:29:45 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Awair Hack v1.0.0
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
